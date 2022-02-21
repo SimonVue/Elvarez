de91ced519477a22f43eefd85a008d99a46e4e0d
SimonVue <viktorov_@mail.ru>
2022-02-21 10:03:18 +0300

    add menu-burger

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* ========================= Burger ======================== */
+/* ========================= Burger - menu ======================== */
 document.querySelector('.menu-burger').onclick = function () {
   open()
 };
@@ -27,18 +27,42 @@ for (let i = 0; i < acc.length; i++) {
 
 /* ========================= Header_menu ======================== */
 
-const btn = document.getElementsByClassName("header-menu__button");
-const openMenu = document.querySelector('.header-menu__panel');
+// document.getElementById('headerOne').onclick = function () {
+//   open()
+// };
+
+// function open() {
+//   document.body.classList.toggle('_lock');
+//   document.querySelector('.header_menu').classList.toggle('open');
+// }
+
+/* =========================  ======================== */
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn = document.getElementsByClassName("header-menu__button");
+// const openMenu = document.querySelector('.header-menu__panel');
 //const i;
 
-for (let i = 0; i < acc.length; i++) {
-  btn[i].addEventListener("click", function () {
-    openMenu.classList.toggle("open");
-    let panel = openMenu.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+
+
+// for (let i = 0; i < acc.length; i++) {
+//   btn[i].addEventListener("click", function () {
+//     openMenu.classList.toggle("open");
+//     let panel = openMenu.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
